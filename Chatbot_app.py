d590cdb2d504d6155c1bec3b57047e92b583b8b5
MohammoD2 <ibr500037@gmail.com>
2025-11-08 17:21:49 +0600

    adding personal chatbot for alloftech

--- Chatbot_app.py
+++ Chatbot_app.py
@@ -9,7 +9,7 @@ from Chatbot import chatbot   # assuming you saved your code in chatbot.py
 load_dotenv()
 
 # --- Logo Path ---
-logo_path = "images\Logo.jpg"  # ✅ Use your uploaded logo
+logo_path = "https://www.alloftech.site/images/Logo.jpg"  # ✅ Use your uploaded logo
 
 # --- Page Configuration ---
 st.set_page_config(
